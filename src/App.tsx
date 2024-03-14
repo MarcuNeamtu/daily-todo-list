962df806d0128add849fc3d27d1e622204ff920c
MarcuNeamtu <149612588+MarcuNeamtu@users.noreply.github.com>
2024-03-14 22:28:06 +0000

    defin todo function

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import TodoForm from './components/TodoForm';
 
 import 'normalize.css';
 type Todo = {
+  id: number;
   name: string;
   completed: boolean;
   createdAt: Date;
@@ -16,12 +17,23 @@ export default function App() {
 
   const createTodo = (name: string) => {
     const newTodo = {
+      id: Date.now(),
       name: name,
       completed: false,
       createAt: new Date()
     };
     setTodos([...todos, newTodo])
   }  
+
+  const completeTodo = (id: number) => {
+    const index = todos.findIndex(todo => todo.id === id);
+    if (index === -1) return;
+
+    const newTodos = [...todos];
+    const todo = newTodo[index];
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
   
   return (
     <Layout>
